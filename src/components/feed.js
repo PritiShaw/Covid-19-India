000e571148f7ad256b10d0d31970a59cec2af53d
Priti Shaw <pritishaw0103@gmail.com>
2020-03-16 01:39:51 +0530

    Add Feed Section

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,11 +1,39 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-export default class FeedComponent extends React.Component {
-  render () {
-    return (
+export default () => (
+  <StaticQuery
+    query={graphql`
+    {
+      allNewsFeedCsv {
+        edges {
+          node {
+            Title
+            Description
+            SourceName
+            Url
+            PublishedAt(formatString: "MMMM DD, YY")
+          }
+        }
+      }
+    }
+    `}
+    render={data => (
       <>
-        Feed
-      </>
+        {data.allNewsFeedCsv.edges.map((edge) => (
+                          <div>
+                            <h3><a href={ edge.node.Url}>{edge.node.Title}</a></h3>
+                            <small>{edge.node.PublishedAt}</small>
+                            <p>{edge.node.Description}
+                            <br/>
+                            <small><em>by {edge.node.SourceName}</em></small>
+                            </p>
+                          </div>
+                        )
+                      )
+                    }
+      </>              
     )
-  }
-}
+    }
+  />
+)
